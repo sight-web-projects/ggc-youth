561af7a4482db4cf72f1a6d4e525d6c2766f74ef
kisakyamukama <you@efitzerkisakyamukama03@gmail.com>
2020-03-19 00:48:29 +0300

    add libraris add navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import Home  from './components/Home'
+import About from  './components/About';
+import  Navigation from './components/Navigation';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-         Glorious Church of Christ Youths
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Navigation />
+        <Switch>
+          <Route  path="/" component={Home} exact />
+          <Route  path="/about" component={About} />
+          <Route  path="/contact" component={Contact}/>
+          <Route  component={Error} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
