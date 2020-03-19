c7f1537d630921d203c013c88acfad0405736d9d
kisakyamukama <you@efitzerkisakyamukama03@gmail.com>
2020-03-19 08:53:58 +0300

    add navigation

---
+++ src/components/Departments.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Departments = () => {
+    return (
+   <div>
+       <h1>Departments</h1>
+       - choir 
+       - ushering & hospitality
+       - programming 
+       - Events and Project management
+       - 
+   </div>
+    );
+}
+
+export default Departments;
